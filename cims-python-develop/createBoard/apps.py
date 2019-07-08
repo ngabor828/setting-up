60cefd8a63689fa200e3e86e69e4c0271308cf4c
gabor <gabor.nagy@cognitivecreators.com>
2019-07-08 12:39:15 +0300

    teszt

---
+++ cims-python-develop/createBoard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CreateboardConfig(AppConfig):
+    name = 'createBoard'
---
+++ cims-python-develop/jiraNOApi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JiranoapiConfig(AppConfig):
+    name = 'jiraNOApi'
